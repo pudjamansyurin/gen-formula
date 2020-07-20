0886526d72344a4edb85e46f47630a5c06e17858
Pudja Mansyurin <pujakusumae@hotmail.com>
2020-07-20 09:16:04 +0700

    Secured FormulaPercent authorization

--- app/Http/Controllers/FormulaPercentController.php
+++ app/Http/Controllers/FormulaPercentController.php
@@ -22,63 +22,59 @@ class FormulaPercentController extends Controller
      */
     public function store(FormulaPercentRequest $request, Formula $formula)
     {
-        if ($formula) {
-            $formulaNew = collect($request->formula)
-                ->map(function ($el) use ($formula) {
-                    return [
-                        'formula_id' => $formula->id,
-                        'product_id' => $el['product_id'],
-                        'percent' => $el['percent'],
-                        'user_id' => auth()->id(),
-                    ];
-                });
+        $this->authorize('update', $formula);
 
-            // check total
-            if ($formulaNew->sum('percent') == 100) {
-                // check existing
-                $same = [];
-                $changed = [];
-                $formulaNew->each(function ($el, $key) use ($formula, &$same, &$changed) {
-                    $old = $formula->percents->firstWhere('product_id', $el['product_id']);
-                    if ($old) {
-                        // check is percent same
-                        if ($old->percent == $el['percent']) {
-                            array_push($same, $old->product_id);
-                        } else {
-                            array_push($changed, $old->product_id);
-                        }
+        $formulaNew = collect($request->formula)
+            ->map(function ($el) use ($formula) {
+                return [
+                    'formula_id' => $formula->id,
+                    'product_id' => $el['product_id'],
+                    'percent' => $el['percent'],
+                    'user_id' => auth()->id(),
+                ];
+            });
+
+        // check total
+        if ($formulaNew->sum('percent') == 100) {
+            // check existing
+            $same = [];
+            $changed = [];
+            $formulaNew->each(function ($el, $key) use ($formula, &$same, &$changed) {
+                $old = $formula->percents->firstWhere('product_id', $el['product_id']);
+                if ($old) {
+                    // check is percent same
+                    if ($old->percent == $el['percent']) {
+                        array_push($same, $old->product_id);
+                    } else {
+                        array_push($changed, $old->product_id);
                     }
+                }
+            });
+            // delete removed
+            $formula->percents()
+                ->whereNotIn('product_id', array_merge($same, $changed))
+                ->delete();
+            // update existing
+            $formulaNew->whereIn('product_id', $changed)
+                ->each(function ($el) use ($formula) {
+                    $formula->percents()
+                        ->where('product_id', $el['product_id'])
+                        ->update($el);
                 });
-                // delete removed
-                $formula->percents()
-                    ->whereNotIn('product_id', array_merge($same, $changed))
-                    ->delete();
-                // update existing
-                $formulaNew->whereIn('product_id', $changed)
-                    ->each(function ($el) use ($formula) {
-                        $formula->percents()
-                            ->where('product_id', $el['product_id'])
-                            ->update($el);
-                    });
-                // create new
-                $formulaNew->whereNotIn('product_id', array_merge($same, $changed))
-                    ->each(function ($el) use ($formula) {
-                        $formula->percents()->create($el);
-                    });
+            // create new
+            $formulaNew->whereNotIn('product_id', array_merge($same, $changed))
+                ->each(function ($el) use ($formula) {
+                    $formula->percents()->create($el);
+                });
 
-                return response(
-                    new FormulaItem($formula->refresh()),
-                    Response::HTTP_CREATED
-                );
-            }
-
-            throw ValidationException::withMessages([
-                'total_percentage' => 'The total percentage should be 100%',
-            ]);
+            return response(
+                new FormulaItem($formula->refresh()),
+                Response::HTTP_CREATED
+            );
         }
 
-        return response([
-            'message' => 'Formula category not found'
-        ], Response::HTTP_NOT_FOUND);
+        throw ValidationException::withMessages([
+            'total_percentage' => 'The total percentage should be 100%',
+        ]);
     }
 }
