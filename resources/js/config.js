5fe51ee8fd60a0c51c01efb07c3d1f54f86abbb8
Pudja Mansyurin <pujakusumae@hotmail.com>
2020-07-23 10:31:17 +0700

    Change vue app domain

--- resources/js/config.js
+++ resources/js/config.js
@@ -1,5 +1,5 @@
 export default {
     APP_NAME: process.env.MIX_APP_NAME,
-    APP_URL: process.env.MIX_APP_URL
-    // DEBUG: process.env.MIX_APP_DEBUG
+    APP_URL: process.env.MIX_APP_URL,
+    DEBUG: process.env.MIX_APP_DEBUG
 };
