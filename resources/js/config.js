7f1f39512b91c219953a8c45114f2b559a2d6459
Pudja Mansyurin <pujakusumae@hotmail.com>
2020-07-23 10:48:52 +0700

    Fix js config.debug bug

--- resources/js/config.js
+++ resources/js/config.js
@@ -1,5 +1,5 @@
 export default {
     APP_NAME: process.env.MIX_APP_NAME,
     APP_URL: process.env.MIX_APP_URL,
-    DEBUG: process.env.MIX_APP_DEBUG
+    DEBUG: process.env.MIX_APP_DEBUG === "true"
 };
