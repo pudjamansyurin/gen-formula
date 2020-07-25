9be06780d5ea790ae517be7f12db683e5c8a7f8c
Pudja Mansyurin <pujakusumae@hotmail.com>
2020-07-25 12:12:11 +0700

    Change how webpack handle environtment

--- resources/js/utils/config.js
+++ resources/js/utils/config.js
@@ -1,5 +1,10 @@
+const production = process.env.NODE_ENV === "production";
+
 export default {
     APP_NAME: process.env.MIX_APP_NAME,
-    APP_URL: process.env.MIX_APP_URL,
+    APP_URL: production
+        ? process.env.MIX_APP_PROD_URL
+        : process.env.MIX_APP_URL,
+    ENV_PRODUCTION: production,
     DEBUG: process.env.MIX_APP_DEBUG === "true"
 };
