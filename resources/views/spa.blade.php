ead4dfd5d89ff52e17dc5c5efa278065539b3814
Pudja Mansyurin <pujakusumae@hotmail.com>
2020-07-25 17:33:55 +0700

    Update favicon

--- resources/views/spa.blade.php
+++ resources/views/spa.blade.php
@@ -9,6 +9,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
+    <link href="{{ asset('favicon.ico') }}" rel="shortcut icon" >
 
     <!-- Styles -->
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
