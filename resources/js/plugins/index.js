86235c13d591b63425b9a4329e111f55646154fd
Puja Kusuma <pujakusumae@hotmail.com>
2020-10-25 20:54:23 +0700

    Removed vue-lodash

--- resources/js/plugins/index.js
+++ resources/js/plugins/index.js
@@ -2,7 +2,6 @@ import "./vee-validate";
 import "./vue-currency-filter";
 import "./vue-fragment";
 import "./vue-fullscreen";
-// import "./vue-lodash";
 import "./vue-long-click";
 import "./vue-moment";
 import vuetify from "./vuetify";
