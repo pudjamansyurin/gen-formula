ef46a53711b1f230cb9e6ba79e7503bb9a44047d
Puja Kusuma <pujakusumae@hotmail.com>
2021-02-19 07:19:27 +0700

    Remove unused logger

--- resources/js/layouts/Dashboard.vue
+++ resources/js/layouts/Dashboard.vue
@@ -1,18 +1,17 @@
 <template>
-    <fragment>
-        <app-left-drawer></app-left-drawer>
-        <app-container>
-            <router-view></router-view>
-        </app-container>
-        <app-bottom-nav v-if="mobile"></app-bottom-nav>
-    </fragment>
+  <fragment>
+    <app-left-drawer></app-left-drawer>
+    <app-container>
+      <router-view></router-view>
+    </app-container>
+    <app-bottom-nav v-if="mobile"></app-bottom-nav>
+  </fragment>
 </template>
 
 <script>
 import { CommonMixin } from "@/mixins";
 import { logger } from "@/utils";
 
-import AppTopBar from "@/components/AppTopBar";
 import AppContainer from "@/components/AppContainer";
 import AppBottomNav from "@/components/AppBottomNav";
 import AppLeftDrawer from "@/components/AppLeftDrawer";
@@ -20,14 +19,10 @@ import AppLeftDrawer from "@/components/AppLeftDrawer";
 export default {
     mixins: [CommonMixin],
     components: {
-        AppTopBar,
         AppContainer,
         AppBottomNav,
-        AppLeftDrawer,
-    },
-    mounted() {
-        logger("Session credebility should be checked!");
-    },
+        AppLeftDrawer
+    }
 };
 </script>
 
